1f19d022d46e923bbe82bf1638e16ac668f48e56
schasse <schasse@mailbox.tu-berlin.de>
2013-07-24 00:23:23 +0200

    disable some settings i cant use anymore

--- emacs.d/personal/personal.el
+++ emacs.d/personal/personal.el
@@ -11,26 +11,26 @@
 (scroll-bar-mode -1)
 
 ;; additional packages
-(prelude-ensure-module-deps '(git-gutter-fringe escreen))
+(prelude-ensure-module-deps '(git-gutter escreen))
 
 ;; ruby
 (setq ruby-insert-encoding-magic-comment nil)
 (setq ruby-deep-indent-paren nil)
-(add-hook 'ruby-mode-hook 'linum-mode)
+;; (add-hook 'ruby-mode-hook 'linum-mode)
 
 ;; keybindings
-(global-set-key (kbd "C-#") 'comment-region)
-(global-set-key (kbd "C-'") 'uncomment-region)
-(global-set-key (kbd "C-ö") 'hippie-expand)
+;; (global-set-key (kbd "C-#") 'comment-region)
+;; (global-set-key (kbd "C-'") 'uncomment-region)
+;; (global-set-key (kbd "C-ö") 'hippie-expand)
 (define-key prelude-mode-map (kbd "C-c f") 'projectile-find-file)
 
-;; git-gutter-fringe
-(require 'git-gutter-fringe)
+;; git-gutter
+(require 'git-gutter)
 (global-git-gutter-mode t)
 
 ;; multi-term
-(setq multi-term-program "/bin/zsh")
-(global-set-key (kbd "C-c T") 'multi-term)
-(define-key prelude-mode-map (kbd "C-c t") 'multi-term-next)
+;; (setq multi-term-program "/bin/zsh")
+;; (global-set-key (kbd "C-c T") 'multi-term)
+;; (define-key prelude-mode-map (kbd "C-c t") 'multi-term-next)
 
 ;;; personal.el ends here
