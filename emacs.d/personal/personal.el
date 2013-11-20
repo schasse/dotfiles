42c660aa54e87dea1c89fdbdeb2c573bdac7e337
schasse <mail@sebastianschasse.de>
2013-11-20 23:45:59 +0100

    disable some breaking keybindings of nguyens escreen

--- emacs.d/personal/personal.el
+++ emacs.d/personal/personal.el
@@ -10,10 +10,9 @@
 (blink-cursor-mode t)
 ;;(scroll-bar-mode -1)
 (set-display-table-slot standard-display-table 'wrap ?\ )
-(setq projectile-completion-system 'grizzl)
 
 ;; additional packages
-(prelude-require-packages '(git-gutter escreen))
+(prelude-require-packages '(git-gutter+ escreen))
 
 ;; ruby
 (setq ruby-insert-encoding-magic-comment nil)
@@ -31,13 +30,36 @@
 (define-key prelude-mode-map (kbd "C-M-o") 'prelude-smart-open-line-above)
 (define-key prelude-mode-map (kbd "C-M-j") 'prelude-top-join-line)
 
-;; git-gutter
-(require 'git-gutter)
-(global-git-gutter-mode t)
+;; git-gutter+
+(require 'git-gutter+)
+(global-git-gutter+-mode t)
 
 ;; multi-term
 ;; (setq multi-term-program "/bin/zsh")
 ;; (global-set-key (kbd "C-c T") 'multi-term)
 ;; (define-key prelude-mode-map (kbd "C-c t") 'multi-term-next)
 
+;; Display ido results vertically, rather than horizontally
+(setq ido-decorations (quote("\n-> "
+                             ""
+                             "\n   "
+                             "\n   ..."
+                             "["
+                             "]"
+                             " [No match]"
+                             " [Matched]"
+                             " [Not readable]"
+                             " [Too big]"
+                             " [Confirm]")))
+
+(defun ido-disable-line-truncation ()
+  "Truncate long lines."
+  (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+
+(defun ido-define-keys ()
+  "C-n/p is more intuitive in vertical layout."
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'ido-define-keys)
 ;;; personal.el ends here
