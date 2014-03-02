1be4d4d8b5bb8b88917cb9187ba8c5aabd11a6a6
schasse <mail@sebastianschasse.de>
2014-03-02 18:23:40 +0100

    cleanup personal.el

--- emacs.d/personal/personal.el
+++ emacs.d/personal/personal.el
@@ -14,6 +14,12 @@
 ;; additional packages
 (prelude-require-packages '(git-gutter+ escreen rvm auto-complete robe))
 
+;; keybindings
+(define-key prelude-mode-map (kbd "C-c f") 'projectile-find-file)
+(define-key prelude-mode-map (kbd "C-M-o") 'prelude-smart-open-line-above)
+(define-key prelude-mode-map (kbd "C-M-j") 'prelude-top-join-line)
+(define-key prelude-mode-map (kbd "C-M-e") 'er/expand-region)
+
 ;; autocomplete
 (require 'auto-complete-config)
 (ac-config-default)
@@ -21,43 +27,6 @@
 (setq ac-trigger-key "M-/")
 (setq ac-use-quick-help nil)
 (setq ac-auto-start nil)
-
-;; ruby
-(setq ruby-insert-encoding-magic-comment nil)
-(setq ruby-deep-indent-paren nil)
-
-(eval-after-load 'ruby-tools
-  '(progn
-       (define-key ruby-tools-mode-map (kbd "C-c '")
-         'ruby-tools-to-single-quote-string)
-       (define-key ruby-tools-mode-map (kbd "C-c \"")
-         'ruby-tools-to-double-quote-string)
-       (define-key ruby-tools-mode-map (kbd "C-c :")
-         'ruby-tools-to-symbol)
-       (define-key ruby-tools-mode-map (kbd "C-c ;")
-         'ruby-tools-clear-string)))
-
-(add-hook 'ruby-mode-hook 'robe-mode)
-(add-hook 'robe-mode-hook 'robe-ac-setup)
-
-;; js
-(setq js-indent-level 2)
-(setq coffee-tab-width 2)
-(add-hook 'coffee-mode-hook
-          (lambda ()
-            (subword-mode +1)))
-
-;; sh
-(setq sh-basic-offset 2)
-
-;; keybindings
-;; (global-set-key (kbd "C-#") 'comment-region)
-;; (global-set-key (kbd "C-'") 'uncomment-region)
-;; (global-set-key (kbd "C-ö") 'hippie-expand)
-(define-key prelude-mode-map (kbd "C-c f") 'projectile-find-file)
-(define-key prelude-mode-map (kbd "C-M-o") 'prelude-smart-open-line-above)
-(define-key prelude-mode-map (kbd "C-M-j") 'prelude-top-join-line)
-(define-key prelude-mode-map (kbd "C-M-e") 'er/expand-region)
 
 ;; git-gutter+
 (global-git-gutter+-mode t)
@@ -90,4 +59,30 @@
   (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
   (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
 (add-hook 'ido-setup-hook 'ido-define-keys)
+
+;; ruby
+(setq ruby-insert-encoding-magic-comment nil)
+(setq ruby-deep-indent-paren nil)
+
+(eval-after-load 'ruby-tools
+  '(progn
+       (define-key ruby-tools-mode-map (kbd "C-c '")
+         'ruby-tools-to-single-quote-string)
+       (define-key ruby-tools-mode-map (kbd "C-c \"")
+         'ruby-tools-to-double-quote-string)
+       (define-key ruby-tools-mode-map (kbd "C-c :")
+         'ruby-tools-to-symbol)
+       (define-key ruby-tools-mode-map (kbd "C-c ;")
+         'ruby-tools-clear-string)))
+
+(add-hook 'ruby-mode-hook 'robe-mode)
+(add-hook 'robe-mode-hook 'robe-ac-setup)
+
+;; js
+(setq js-indent-level 2)
+(setq coffee-tab-width 2)
+(add-hook 'coffee-mode-hook
+          (lambda ()
+            (subword-mode +1)))
+(add-to-list 'ac-modes 'coffee-mode)
 ;;; personal.el ends here
