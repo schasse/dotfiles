7cbaf4a6dc8116ab8b6b8a5c21ac30bd65bba4d0
schasse <mail@sebastianschasse.de>
2018-08-23 16:54:11 +0200

    (Made it to compile...)

--- symlinked/emacs.d/personal/custom.el
+++ symlinked/emacs.d/personal/custom.el
@@ -8,6 +8,9 @@
    (quote
     ("f61972772958e166cda8aaf0eba700aad4faa0b4101cee319e894e7a747645c9" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(org-agenda-files nil)
+ '(package-selected-packages
+   (quote
+    (zop-to-char zenburn-theme yari yaml-mode which-key web-mode volatile-highlights vlf undo-tree smex smartrep smartparens smart-mode-line scss-mode scala-mode2 rvm ruby-tools rspec-mode robe rainbow-mode rainbow-delimiters ov operate-on-number multiple-cursors move-text markdown-mode magit-gh-pulls latex-preview-pane latex-extra key-chord json-mode imenu-anywhere ido-vertical-mode ido-ubiquitous hl-todo helm-projectile haml-mode guru-mode grizzl god-mode go-mode gitignore-mode gitconfig-mode git-timemachine git-gutter+ gist geiser flycheck flx-ido expand-region ess escreen ensime emmet-mode elisp-slime-nav ein-mumamo editorconfig-core editorconfig easy-kill dockerfile-mode discover-my-major diminish diff-hl d-mode csv-mode crux counsel company-auctex company-anaconda coffee-mode cdlatex bundler build-status browse-kill-ring beacon anzu ag ace-window)))
  '(show-paren-mode t)
  '(size-indication-mode t)
  '(tool-bar-mode nil))
