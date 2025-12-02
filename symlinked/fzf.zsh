c1b1980394de8c37f1308d81b1c1edb96c2db6db
schasse <mail@sebastianschasse.de>
2025-12-02 18:05:10 +0100

    update fzf

--- symlinked/fzf.zsh
+++ symlinked/fzf.zsh
@@ -4,10 +4,4 @@ if [[ ! "$PATH" == */Users/schasse/.fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/Users/schasse/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-source "/Users/schasse/.fzf/shell/completion.zsh"
-
-# Key bindings
-# ------------
-source "/Users/schasse/.fzf/shell/key-bindings.zsh"
+source <(fzf --zsh)
